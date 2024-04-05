f4834d38eed160c66dd9c86192d7a332139e903d
SkyeJayleen <skyejayleen@gmail.com>
2024-04-05 11:05:11 -0400

    almost there...

--- munge.py
+++ munge.py
@@ -21,7 +21,6 @@ def isolate(namelist, allnames):
             continue
     return allnames
 
-
 # Delete columns
 def del_column(columnname):
     for i in data:
@@ -29,6 +28,18 @@ def del_column(columnname):
             del i[j]
     return data
 
+# Clean special characters in each column: commas(,) in neighborhoods that interferes in parsing
+def replacecomma(column):
+    for i in data:
+        i[column] = i[column].replace(",", ";")
+
+# Clean dollar sign($) in price and turn them into integer values for easier aggregation
+def cleanprice(column):
+    for i in data:
+        i[column] = i[column].replace(",", "")
+        i[column] = i[column].replace("$", "")
+        i[column] = i[column].replace(".00", "")
+        
 # Save data
 def save_csv_data(data, filepath):
     file = open(filepath, 'w', encoding = 'utf-8')
@@ -39,11 +50,19 @@ def save_csv_data(data, filepath):
         file.write('\n')
     file.close()
 
+# Retrieve data
 data = get_csv("data/listings.csv")
+
+# Remove columns
 namelist = ['id', 'name', 'price', 'neighbourhood', 'host_name', 'host_is_superhost', 'host_id', 'beds', 'neighbourhood_group_cleansed', 'review_scores_rating']
 allnames = list(data[0].keys())
 
 noneed = isolate(namelist, allnames)
 del_column(noneed)
 
+# Clean special characters
+replacecomma("neighbourhood")
+cleanprice("price")
+
+# Save data
 save_csv_data(data, "data/listings_clean.csv")
